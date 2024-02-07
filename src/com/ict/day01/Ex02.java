22f831d4e61e0f1327d351a5390356b1e77ff186
picom0022 <picom0022@gmail.com>
2024-02-07 10:56:01 +0900

    school-2

--- src/com/ict/day01/Ex02.java
+++ src/com/ict/day01/Ex02.java
@@ -3,5 +3,10 @@ package com.ict.day01;
 public class Ex02 {
 	public static void main(String[] args) {
 		System.out.println("Ex02 Create");
+		System.out.println("Ex02 Update");
+		System.out.println("Ex02 Update");
+		System.out.println("Ex02 Update");
+		System.out.println("Ex02 Update");
+		System.out.println("Ex02 Update");
 	}
 }
